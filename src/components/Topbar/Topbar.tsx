3779d828d1d2cc685504426032465359cec3811f
Apriza Prasetio - DAD Partner <AprizaP@unitedtractors.com>
2024-03-17 08:56:41 +0700

    💄 Redesigned a whole home page

---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,14 @@
+function Topbar() {
+  return (
+    <>
+      <div className="flex justify-end px-3 py-1 text-sm font-bold bg-purple-100">
+        #StandWithPalestine 🍉
+      </div>
+      <nav className="sticky top-0 z-50 px-8 py-4 bg-white shadow-md">
+        <h1 className="text-lg font-semibold">Apriza Prasetio</h1>
+      </nav>
+    </>
+  );
+}
+
+export default Topbar;
