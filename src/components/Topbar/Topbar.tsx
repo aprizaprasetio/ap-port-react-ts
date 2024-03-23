60896bf81084513122593c7c055206a56ff5dfaa
Apriza Prasetio - DAD Partner <AprizaP@unitedtractors.com>
2024-03-23 16:16:08 +0700

    💄 Integrated top bar with tanstack router

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@tanstack/react-router";
+
 function Topbar() {
   return (
     <>
@@ -5,7 +7,9 @@ function Topbar() {
         #StandWithPalestine 🍉
       </div>
       <nav className="sticky top-0 z-50 px-8 py-4 bg-white shadow-md">
-        <h1 className="text-lg font-semibold">Apriza Prasetio</h1>
+        <h1 className="text-lg font-semibold text-slate-800">
+          <Link to="/">Apriza Prasetio</Link>
+        </h1>
       </nav>
     </>
   );
