59d160dcef906a40d96c90f6e4c4749340fcc927
Apriza Prasetio - DAD Partner <AprizaP@unitedtractors.com>
2024-03-23 16:23:12 +0700

    🔧 Removed vite plugin svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
 import unpluginInjectPreload from 'unplugin-inject-preload/vite'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
